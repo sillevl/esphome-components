51d6e429d3bfa231b560f57cfcb43b82aa1cbb6a
Sille Van Landschoot <info@sillevl.be>
2024-11-09 11:33:52 +0100

    add voltage calculation

--- components/kalfire/kalfire.cpp
+++ components/kalfire/kalfire.cpp
@@ -1,10 +1,33 @@
 #include "kalfire.h"
+#include <algorithm>
 
 namespace esphome {
 namespace kalfire {
 
+const float Kalfire::MIN_FLAME_HEIGHT_VOLTAGE = 3.0;
+const float Kalfire::MAX_FLAME_HEIGHT_VOLTAGE = 9.0;
+const uint8_t Kalfire::MIN_FLAME_HEIGHT = 0;
+const uint8_t Kalfire::MAX_FLAME_HEIGHT = 7;
+
 Kalfire::Kalfire() {
     // Constructor
+}
+
+float Kalfire::get_voltage() {
+    if(!this->power_on) { return 0; }
+    if(this->eco_mode) { return 95; }
+
+    return this->flame_height;
+}
+
+float Kalfire::flame_height_to_voltage(uint8_t flame_height) {
+    flame_height = std::min(flame_height, this->MAX_FLAME_HEIGHT);
+    flame_height = std::max(flame_height, this->MIN_FLAME_HEIGHT);
+
+    float a = MIN_FLAME_HEIGHT_VOLTAGE;
+    float b = (MAX_FLAME_HEIGHT_VOLTAGE - MIN_FLAME_HEIGHT_VOLTAGE) / MAX_FLAME_HEIGHT;
+
+    return a + (b * flame_height);
 }
 
 } // namespace kalfire
--- components/kalfire/kalfire.h
+++ components/kalfire/kalfire.h
@@ -11,6 +11,18 @@ class Kalfire : public Component {
     public:
         Kalfire();
 
+    private:
+        float flame_height_to_voltage(uint8_t flame_height);
+
+    protected:
+        float get_voltage();
+        uint8_t flame_height = 0;
+        bool power_on = false;
+        bool eco_mode = false;
+        static const float MAX_FLAME_HEIGHT_VOLTAGE;
+        static const float MIN_FLAME_HEIGHT_VOLTAGE;
+        static const uint8_t MAX_FLAME_HEIGHT;
+        static const uint8_t MIN_FLAME_HEIGHT;
 };
 
 } // namespace status_led
